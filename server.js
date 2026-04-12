f1c8e3330602566fcd9707b382aca5f1237f74b9
JRobles312 <jesus@fxrcon.com>
2026-04-12 14:45:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const JOBTREAD_API = "https://api.jobtread.com/pave";
